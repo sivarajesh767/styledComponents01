ce43884ab65d890d973aae318a61020b654ca18d
sivarajesh767 <sivarajesh767@gmail.com>
2024-11-04 03:34:12 +0000

    Please check Error

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css'
+import {Heading} from './styledComponents'
+
+const App = () => (
+  <>
+    <Heading>Hello World</Heading>
+  </>
+)
+export default App
---
+++ src/styledComponents.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Heading = styled.h1`
+font-family:'Roboto';
+font-weight:800;
+font-size:65px;
+
+`;
